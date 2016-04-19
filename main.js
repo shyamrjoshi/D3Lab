2cbba0a4af00227795b81d651817b01a9a3e31eb
Shyam Joshi <shyamrjoshi@yahoo.com>
2016-04-19 13:36:12 -0400

    changes to scatterplot
    updated query mpg function

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
-	 xcol = 1, // active x column
-     ycol = 1; // active y column
 //setup x and y	  
- var x = d3.scale.linear().range([0, width]);
- var y = d3.scale.linear().range([height, 0]);
- var xAxis = d3.svg.axis().scale(x).orient("bottom");
- var yAxis = d3.svg.axis().scale(y).orient("left");
+var x = d3.scale.linear().range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
+var xAxis = d3.svg.axis().scale(x).orient("bottom");
+var yAxis = d3.svg.axis().scale(y).orient("left");
+currentX = 1, 
+currentY = 1; 
 // setup fill color
 var cValue = function(d) { return d.origin;},
     color = d3.scale.category10();
@@ -30,14 +30,14 @@ var tooltip = d3.select("h4").append("hovered")
 // load data
 d3.csv("car.csv", function(error, data) {
 
- var headers = Object.keys(data[0]).slice(1, -1);
+ var columnsInTable = Object.keys(data[0]).slice(1, -1);
 // Dynamic dropdown menu
    $("select").each(function(_, dropdown) {
-     $.each(headers, function(_, name) {
+     $.each(columnsInTable, function(_, name) {
        $(dropdown).append($("<option></option>").attr("value",name).text(name));
      });
    });
-   var xLabel = svg.append("g")
+   var xAxisTitle = svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis)
@@ -46,7 +46,7 @@ d3.csv("car.csv", function(error, data) {
       .attr("x", width)
       .attr("y", -6)
       .style("text-anchor", "end");
-   var yLabel = svg.append("g")
+   var yAxisTitle = svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
       .append("text")
@@ -56,33 +56,33 @@ d3.csv("car.csv", function(error, data) {
       .attr("dy", ".71em")
       .style("text-anchor", "end");
 // display the scatterplot  
-   var scatterPlotDisplay = function(filterMin, filterMax) {
-     var xCol = $("#xaxis").val();
-     var yCol = $("#yaxis").val();
-     var min = filterMin || Number.MIN_VALUE;
-     var max = filterMax || Number.MAX_VALUE;
-     var filteredData = data.filter(function(car) {
+   var scatterPlotDisplay = function(rangeStart, rangeEnd) {
+     var currentX = $("#sel-x").val();
+     var currentY = $("#sel-y").val();
+     var min = rangeStart || Number.MIN_VALUE;
+     var max = rangeEnd || Number.MAX_VALUE;
+     var new_data = data.filter(function(car) {
        return +car.mpg >= min && +car.mpg <= max;
      });
-     x.domain(d3.extent(filteredData, function(d) { return +d[xCol]; })).nice();
-     y.domain(d3.extent(filteredData, function(d) { return +d[yCol]; })).nice();
+     x.domain(d3.extent(new_data, function(d) { return +d[currentX]; })).nice();
+     y.domain(d3.extent(new_data, function(d) { return +d[currentY]; })).nice();
      svg.selectAll("g .x.axis").call(xAxis);
      svg.selectAll("g .y.axis").call(yAxis);
-     xLabel.text(xCol);
-     yLabel.text(yCol);
-     var dots = svg.selectAll(".dot").data(filteredData);
+     xAxisTitle.text(currentX);
+     yAxisTitle.text(currentY);
+     var dots = svg.selectAll(".dot").data(new_data);
      var t = d3.transition().duration(200).style("opacity", .9);
      dots.transition()
-         .attr("cx", function(d) { return x(+d[xCol]); })
-         .attr("cy", function(d) { return y(+d[yCol]); })
+         .attr("cx", function(d) { return x(+d[currentX]); })
+         .attr("cy", function(d) { return y(+d[currentY]); })
 		 .duration(1000).style("opacity", .9);
      dots.enter()
          .append("circle")
          .attr("class", "dot")
          .attr("r", 3.5)
 		 .style("fill", function(d) { return color(cValue(d));}) 
-         .attr("cx", function(d) { return x(+d[xCol]); })
-         .attr("cy", function(d) { return y(+d[yCol]); })
+         .attr("cx", function(d) { return x(+d[currentX]); })
+         .attr("cy", function(d) { return y(+d[currentY]); })
          .on("mouseover", function(d) { setTitle(d['name']) })
          .on("mouseout", function(d) { setTitle() });
      dots.exit().remove();
